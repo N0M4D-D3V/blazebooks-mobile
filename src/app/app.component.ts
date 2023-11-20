15591c755659babed96edea1f91c3b460ee634a1
N0M4D <victor.develops@gmail.com>
2023-11-20 19:28:27 +0100

    toolbar first implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { DemiToolbarConfig } from 'demiurge';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public readonly toolbarConfig: DemiToolbarConfig = {
+    title: 'Blazebooks',
+    toggleable: true,
+    toggleTitle: 'Blazebooks',
+  };
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemiToolbarModule } from 'demiurge';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, DemiToolbarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

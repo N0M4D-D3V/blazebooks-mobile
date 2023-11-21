88dd0387251a9ee8b82d9d236bcf3409973689da
N0M4D <victor.develops@gmail.com>
2023-11-21 19:42:08 +0100

    demo login and routing to home page

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { LoginPage } from './login.page';
 import { CommonModule } from '@angular/common';
 import { LoginPageRoutingModule } from './login-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, LoginPageRoutingModule],
+  imports: [CommonModule, FormsModule, LoginPageRoutingModule],
   exports: [LoginPage],
   declarations: [LoginPage],
 })
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: [],
 })
-export class LoginPage {}
+export class LoginPage {
+  username: string = '';
+  password: string = '';
+
+  constructor(private readonly router: Router) {}
+
+  loginUser() {
+    console.log(`Username: ${this.username}, Password: ${this.password}`);
+    if (this.username === 'user' && this.password === 'pass')
+      this.router.navigate(['/home']);
+  }
+}

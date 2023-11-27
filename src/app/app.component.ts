528549a131372a1ea9fcac505dcd40cf58a8391c
N0M4D <victor.develops@gmail.com>
2023-11-27 19:21:09 +0100

    lateral menu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { DemiToolbarConfig } from 'demiurge';
 
 @Component({
@@ -11,5 +12,16 @@ export class AppComponent {
     title: 'Blazebooks',
     toggleable: true,
     toggleTitle: 'Blazebooks',
+    items: [
+      { label: 'home', url: '/home', icon: 'bi-house' },
+      { label: 'search', url: '/search', icon: 'bi-search' },
+    ],
   };
+
+  constructor(private readonly router: Router) {}
+
+  public onLogout(): void {
+    console.log('que pasa');
+    this.router.navigate(['/login']);
+  }
 }

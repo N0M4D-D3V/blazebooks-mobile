26c72367d422ea0ba42e44023b76e4ccc09c7842
N0M4D <victor.develops@gmail.com>
2023-12-02 15:15:35 +0100

    demiurge toolbar service now manages the search values without use any kind of external inicialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import {
-  DemiToolbarComponent,
-  DemiToolbarConfig,
-  DemiToolbarService,
-} from 'demiurge';
+import { DemiToolbarComponent, DemiToolbarConfig } from 'demiurge';
 import { TOOLBAR_CONFIG } from '@config/toolbar.config';
 import { RoutePath } from './interfaces/route.interface';
 
@@ -16,7 +12,6 @@ import { RoutePath } from './interfaces/route.interface';
     <demi-toolbar
       [config]="toolbarConfig"
       (onLogout)="onLogout()"
-      (onSearch)="onSearch($event)"
     ></demi-toolbar>
     <router-outlet></router-outlet>
   `,
@@ -24,14 +19,7 @@ import { RoutePath } from './interfaces/route.interface';
 export class AppComponent {
   public readonly toolbarConfig: DemiToolbarConfig = TOOLBAR_CONFIG;
 
-  constructor(
-    private readonly router: Router,
-    private readonly demiToolbarService: DemiToolbarService
-  ) {}
-
-  public onSearch(ev: string): void {
-    this.demiToolbarService.searchIn(ev);
-  }
+  constructor(private readonly router: Router) {}
 
   public onLogout(): void {
     this.router.navigate([RoutePath.Login]);

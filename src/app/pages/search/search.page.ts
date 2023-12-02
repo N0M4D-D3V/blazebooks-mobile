3102c1ca9df8d05ec0c77f2539cc9b0b66fcf6a0
N0M4D <victor.develops@gmail.com>
2023-12-02 19:12:27 +0100

    <> search view updated with new demiurge card logic <>

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,17 +1,20 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-  DemiCardComponent,
-  DemiCardConfig,
-  DemiCardListComponent,
-} from 'demiurge';
-import { Observable, Subscription } from 'rxjs';
-import { BookService } from 'src/app/services/book.service';
+import { Book } from '@interfaces/book.interface';
+import { DemiCardConfig, DemiCardListComponent, DemiCardSize } from 'demiurge';
+import { Observable, Subscription, map } from 'rxjs';
+import { BookService } from '@services/book.service';
+import { Router } from '@angular/router';
+import { RoutePath } from '@interfaces/route.interface';
 
 @Component({
   selector: 'app-search',
   template: `
-    <demi-card-list [config$]="books$" [canSearch]="true"></demi-card-list>
+    <demi-card-list
+      [config$]="books$"
+      [canSearch]="true"
+      (onCardTouched)="onCardTouched($event)"
+    ></demi-card-list>
   `,
   styleUrls: [],
   standalone: true,
@@ -19,12 +22,28 @@ import { BookService } from 'src/app/services/book.service';
 })
 export class SearchPage implements OnInit, OnDestroy {
   private subBooks!: Subscription;
-  public books$: Observable<DemiCardConfig[]> = this.bookService.books$;
+  public books$: Observable<DemiCardConfig<Book>[]> =
+    this.bookService.books$.pipe(
+      map((x) => {
+        return x.map((book) => {
+          book.size = DemiCardSize.M;
+          book.isClickable = true;
+          return book;
+        });
+      })
+    );
 
-  constructor(private readonly bookService: BookService) {}
+  constructor(
+    private readonly router: Router,
+    private readonly bookService: BookService
+  ) {}
 
   ngOnInit(): void {
     this.subBooks = this.books$.subscribe();
+  }
+
+  public onCardTouched(card: DemiCardConfig<Book>): void {
+    this.router.navigate([RoutePath.Detail]);
   }
 
   ngOnDestroy(): void {

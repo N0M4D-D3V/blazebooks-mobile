76f16ef5f66bc7ee4a626736037207fda11d91c7
N0M4D <victor.develops@gmail.com>
2023-12-02 19:07:58 +0100

    detail page created

--- src/app/pages/detail/detail.page.ts
+++ src/app/pages/detail/detail.page.ts
@@ -1,9 +1,48 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Book } from '@interfaces/book.interface';
+import { RoutePath } from '@interfaces/route.interface';
+import { BookService } from '@services/book.service';
+import {
+  DemiCardConfig,
+  DemiCardImgComponent,
+  DemiCardListComponent,
+  DemiCardSize,
+  DemiSeparePipe,
+} from 'demiurge';
+import { Observable, of } from 'rxjs';
 
 @Component({
-    selector: 'app-detail',
-    template: `<h1 class="text-center">Detail Page</h1>`,
-    styleUrls: [],
-    standalone: true,
+  selector: 'app-detail',
+  template: `
+    <demi-card-img [config]="selectedCard"></demi-card-img>
+    <p class="genre-text text-end px-3">
+      <b>Genres: </b> {{ selectedCard.data?.genres | separe }}
+    </p>
+    <div class="container px-3">
+      <p>{{ selectedCard.data?.description }}</p>
+    </div>
+    <demi-card-list [config$]="related"></demi-card-list>
+  `,
+  styleUrls: ['./detail.page.scss'],
+  standalone: true,
+  imports: [DemiCardListComponent, DemiCardImgComponent, DemiSeparePipe],
 })
-export class DetailPage {}
+export class DetailPage {
+  public selectedCard: DemiCardConfig<Book> = this.bookService.getCurrentBook();
+  public related: Observable<DemiCardConfig<Book>[]> = of(
+    this.bookService.getRelatedBooks().map((x) => {
+      x.size = DemiCardSize.S;
+      return x;
+    })
+  );
+
+  constructor(
+    private readonly router: Router,
+    private readonly bookService: BookService
+  ) {}
+
+  public onRead(): void {
+    this.router.navigate([RoutePath.Reader]);
+  }
+}

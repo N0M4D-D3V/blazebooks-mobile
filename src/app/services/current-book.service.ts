e8b3a7a08d8f68ebc8a858069b91b02202e399dd
N0M4D <victor.develops@gmail.com>
2024-01-18 18:23:57 +0100

    BookService divided into BookService and CurrentBookService

---
+++ src/app/services/current-book.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { LocalStorageKey } from '@enum/local-storage.enum';
+import { Book } from '@interfaces/book.interface';
+import { DemiLocalStorageService } from 'demiurge';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class CurrentBookService {
+  private bsCurrentBook: BehaviorSubject<Book | undefined> =
+    new BehaviorSubject<Book | undefined>(undefined);
+  private $currentBook: Observable<Book | undefined> =
+    this.bsCurrentBook.asObservable();
+  private currentBook?: Book | undefined;
+
+  constructor(private readonly localStorage: DemiLocalStorageService) {}
+
+  public $getCurrentBook(): Observable<Book | undefined> {
+    if (!this.currentBook) {
+      const localBook: Book | undefined = this.localStorage.get<Book>(
+        LocalStorageKey.CurrentBook
+      );
+      if (localBook) this.setCurrentBook(localBook);
+    }
+
+    return this.$currentBook;
+  }
+
+  public setCurrentBook(book: Book): void {
+    this.currentBook = { ...book };
+    this.localStorage.save<Book>(LocalStorageKey.CurrentBook, this.currentBook);
+    this.bsCurrentBook.next(this.currentBook);
+  }
+}

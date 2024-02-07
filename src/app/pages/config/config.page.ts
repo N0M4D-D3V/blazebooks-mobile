8f8d3d27260fd41964a86835cb0791f80a7f474b
N0M4D <victor@gg-victor.dev>
2024-02-07 20:50:56 +0100

    reactive form working

--- src/app/pages/config/config.page.ts
+++ src/app/pages/config/config.page.ts
@@ -1,10 +1,48 @@
-import { Component } from "@angular/core";
+import { NgStyle } from "@angular/common";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { Subscription, debounce, debounceTime, delay } from "rxjs";
 
 @Component({
   selector: "app-config",
-  template: `<h1>HELLO THERE!</h1>`,
+  templateUrl: "./config.page.html",
   styleUrls: [],
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgStyle],
 })
-export class ConfigPage {}
+export class ConfigPage implements OnInit, OnDestroy {
+  private subForm!: Subscription;
+
+  public form: FormGroup = this.fb.group({
+    lightFilter: [true],
+    fontSize: [22],
+  });
+
+  public get isLightFilterOn(): boolean {
+    return this.form.get("lightFilter")!.value as boolean;
+  }
+
+  public get fontSize(): number {
+    return this.form.get("fontSize")!.value as number;
+  }
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.subForm = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe(console.log);
+  }
+
+  public onDeleteLocalData(): void {
+    alert("Funcionalidad no implementada. Estamos trabajando en ello =)");
+  }
+
+  public onDeleteLocalBooks(): void {
+    alert("Funcionalidad no implementada. Estamos trabajando en ello =)");
+  }
+
+  ngOnDestroy(): void {
+    this.subForm.unsubscribe();
+  }
+}
